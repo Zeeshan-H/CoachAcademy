cc37f01a090411e2408ed98a6c51b553b07bd26e
Zeeshan-H <zeeshanh184@gmail.com>
2019-10-28 10:34:14 -0700

    Wow

--- Coach Academy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Coach_Academy.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
