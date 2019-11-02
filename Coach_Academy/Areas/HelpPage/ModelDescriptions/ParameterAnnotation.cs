69fcac94bd0986296391101135c0c69a57b091f9
Zeeshan-H <zeeshanh184@gmail.com>
2019-11-01 18:05:25 -0700

    final

---
+++ Coach_Academy/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Coach_Academy.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ Coach_Academy/Controllers/usersController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Net.Http.Headers;
+using System.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+
+
+namespace Coach_Academy.Controllers
+{
+
+    [RoutePrefix("api/users")]
+
+
+
+    public class usersController : ApiController
+    {
+
+
+        [AllowAnonymous]
+
+        [HttpGet]
+
+        public IEnumerable<User> Get()
+        {
+
+            using (coachEntities entities = new coachEntities())
+            {
+
+                return entities.Users.ToList();
+            }
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(User))]
+
+        public IHttpActionResult Post(User tb)
+        {
+
+            coachEntities db = new coachEntities();
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            db.Users.Add(tb);
+            db.SaveChanges();
+            return CreatedAtRoute("DefaultApi", new
+            {
+                id = tb.ID
+
+            }, tb);
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
